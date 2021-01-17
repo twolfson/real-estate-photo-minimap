938e11ba2a850292889d669f4d971ac944750a60
Todd Wolfson <todd@twolfson.com>
2021-01-16 23:32:05 -0800

    Added React successfully

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+// Wrap all our content in an IIFE to avoid leaks
+(function () {
+
+// Load in our dependencies
+const ReactDOM = window.ReactDOM;
+const e = React.createElement;
+
+// Define our main page load hook
+function main() {
+  let reactContainer = document.getElementById('react-content');
+  if (!reactContainer) { throw new Error('Unable to find #react-content'); }
+
+  ReactDOM.render(
+    e('h1', {}, 'foo'),
+    reactContainer
+  );
+}
+// DEV: We could use `DOMContentLoaded` hook but our script location is good enough
+main();
+
 // Model singleton and constants
 // DEV: `evt.key` is a string, this is easier to maintain via Sublime Text mutli-select
 const CATEGORIES = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
@@ -20,3 +40,5 @@ window.addEventListener('keypress', (evt) => {
     Store.nextImage();
   }
 });
+
+}());
