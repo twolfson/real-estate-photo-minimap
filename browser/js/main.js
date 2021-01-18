c98cfc9953c41d5f5059aee7e848741912521ba5
Todd Wolfson <todd@twolfson.com>
2021-01-18 14:51:28 -0800

    Got basic router hook loaded, sans routing

--- browser/js/main.js
+++ browser/js/main.js
@@ -1,13 +1,31 @@
 // Load in our dependencies
+const assert = require('assert');
+const h = require('react-hyperscript');
+const React = require('react');
 const ReactDOM = require('react-dom');
-const {Router, Route} = require('react-router');
+const {BrowserRouter, Switch, Route} = require('react-router-dom');
+
+class Home extends React.Component {
+  render() {
+    return h('h1', 'Welcome to the Home Page');
+  }
+};
 
 // Define our main page load hook
 function main() {
+  // Load our container
   let reactContainer = document.getElementById('react-content');
-  if (!reactContainer) { throw new Error('Unable to find #react-content'); }
+  assert(reactContainer, 'Unable to find #react-content');
+  reactContainer.textContent = 'Attaching content...';
 
-  // TODO: Should use `this.props` instead of `Store` for content
-  ReactDOM.render('foo', reactContainer);
+  // Bind our container to our router
+  ReactDOM.render(
+    h(BrowserRouter, [
+      h(Switch, [
+        h(Route, {path: '/', component: Home}),
+      ])
+    ]),
+    reactContainer
+  );
 }
 main();
