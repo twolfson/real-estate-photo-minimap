8bff3d3068cb8eef2a45d102f159e9f0e34f28c0
Todd Wolfson <todd@twolfson.com>
2021-01-19 11:06:10 -0800

    Got resizable working in standalone

--- browser/js/components/minimap-builder.js
+++ browser/js/components/minimap-builder.js
@@ -3,7 +3,7 @@ const h = require('react-hyperscript');
 const React = require('react');
 const ReactDOM = require('react-dom');
 const Draggable = require('react-draggable');
-const { Resizable } = require('react-resizable');
+const { ResizableBox } = require('react-resizable');
 
 // Fancy importing non-JS assets handling for demo
 // TODO: Remove SVG demo
@@ -30,32 +30,28 @@ class MinimapBuilder extends React.Component {
       let top = Math.floor(i / 5) * 150;
       let width = 100;
       let height = 100;
-      return h(Draggable, {
-        bounds: 'parent',
-        onStart: () => { this.setState({dragging: true}); },
-        onStop: () => { this.setState({dragging: false}); },
-      }, [
-        h(Resizable, {
-        }, [
-          h('div', {
-            style: {
-              // Vertical centering for span, https://css-tricks.com/centering-css-complete-guide/
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
+      // return h(Draggable, {
+        // bounds: 'parent',
+        // onStart: () => { this.setState({dragging: true}); },
+        // onStop: () => { this.setState({dragging: false}); },
+      // }, [
+      return h(ResizableBox, {
+        height, width,
+        style: {
+          // Vertical centering for span, https://css-tricks.com/centering-css-complete-guide/
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
 
-              cursor: this.state.dragging ? 'grabbing' : 'grab',
-              background: 'white',
-              border: '3px solid black',
-              position: 'absolute', left, top,
-              width, height,
-            }
-          }, [
-            h('.d-inline-block.text-center', [
-              h(`.d-inline-block.small.p-1.location-${location.key}-bg`, {
-              }, location.name)
-            ])
-          ])
+          cursor: this.state.dragging ? 'grabbing' : 'grab',
+          background: 'white',
+          border: '3px solid black',
+          position: 'absolute', left, top,
+        }
+      }, [
+        h('.d-inline-block.text-center', [
+          h(`.d-inline-block.small.p-1.location-${location.key}-bg`, {
+          }, location.name)
         ])
       ])
     })));
