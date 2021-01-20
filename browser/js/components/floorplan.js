3d8088dedf0e9d6d9bceb819e1939c950d0aaa7a
Todd Wolfson <todd@twolfson.com>
2021-01-20 14:49:53 -0800

    Cleaned up for new component

--- browser/js/components/floorplan.js
+++ browser/js/components/floorplan.js
@@ -3,8 +3,6 @@ const assert = require('assert');
 const h = require('react-hyperscript');
 const React = require('react');
 const ReactDOM = require('react-dom');
-const Draggable = require('react-draggable');
-const { Resizable, ResizableBox } = require('react-resizable');
 
 // Vendor dependencies
 // TODO: Relocate the entire construction of blueprint3d into its own file
@@ -12,32 +10,14 @@ const { Resizable, ResizableBox } = require('react-resizable');
 const THREE = require('three.js');
 const jQuery = require('blueprint3d/example/js/jquery.js');
 
-// Monkey patch resizable to work with draggable
-//   https://github.com/STRML/react-resizable/issues/18
-//   `div` not required, actually breaks things it seems
-let _resizeHandler = Resizable.prototype.resizeHandler;
-Resizable.prototype.resizeHandler = function () {
-  let resultFn = _resizeHandler.apply(this, arguments);
-  return function (e) {
-    let result = resultFn.apply(this, arguments);
-    e.stopPropagation();
-    return result;
-  };
-};
-
 // Define our component
-class MinimapBuilder extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {count: 0};
-  }
-
+class Floorplan extends React.Component {
   render() {
     return h('div', {
       ref: (containerEl) => { this.containerEl = containerEl; },
       style: {position: 'relative', height: '300px'},
     }, [
-      h('h1', 'hi' + this.state.count)
+      h('h1', 'hi')
     ]);
   }
 
@@ -51,4 +31,4 @@ class MinimapBuilder extends React.Component {
     // Unmount logic goes here
   }
 }
-module.exports = MinimapBuilder;
+module.exports = Floorplan;
