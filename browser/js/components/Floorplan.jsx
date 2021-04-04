4c09ade7dab20a98262ec1593cee952659c5504c
Todd Wolfson <todd@twolfson.com>
2021-04-04 15:29:34 -0700

    Completed react-hyperscript to JSX transition

--- browser/js/components/Floorplan.jsx
+++ browser/js/components/Floorplan.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable quotes,max-len,eqeqeq,space-before-function-paren,padded-blocks,consistent-this,space-infix-ops,space-in-parens,space-before-blocks */
 // Load in our dependencies
 const assert = require('assert');
-const h = require('react-hyperscript');
 const React = require('react');
 const Store = require('../store');
 
@@ -22,14 +21,14 @@ void require('blueprint3d/example/js/blueprint3d.js'); // eslint-disable-line gl
 const BP3D = window.BP3D;
 
 // Helper functions
-const bootstrapIcon = (svgStr, style) => {
-  return h('svg', {
-    xmlns: 'http://www.w3.org/2000/svg', width: '16', height: '16',
-    fill: 'currentColor', viewBox: '0 0 16 16',
-    style: style || null,
-  }, [
-    h('path', {fillRule: "evenodd", d: svgStr}),
-  ]);
+const BootstrapIcon = ({ svgStr, style }) => {
+  return <svg
+    xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+    fill="currentColor" viewBox="0 0 16 16"
+    style={style || null}
+  >
+    <path fillRule="evenodd" d={svgStr} />
+  </svg>;
 };
 
 // Define our component
@@ -40,44 +39,48 @@ class Floorplan extends React.Component {
   }
 
   render() {
-    return h('#floorplanner', {
-      style: {position: 'relative', height: '300px'},
-    }, [
-      h('canvas#floorplanner-canvas'),
-      h('#floorplanner-controls', [
-        h('div', [
-          h('button#move.btn.btn-sm.btn-outline-secondary.mb-1.text-left', {
-            style: {minWidth: '120px'}
-          }, [
-            // https://icons.getbootstrap.com/icons/arrows-move/
-            bootstrapIcon('M7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708l2-2zM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10zM.146 8.354a.5.5 0 0 1 0-.708l2-2a.5.5 0 1 1 .708.708L1.707 7.5H5.5a.5.5 0 0 1 0 1H1.707l1.147 1.146a.5.5 0 0 1-.708.708l-2-2zM10 8a.5.5 0 0 1 .5-.5h3.793l-1.147-1.146a.5.5 0 0 1 .708-.708l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L14.293 8.5H10.5A.5.5 0 0 1 10 8z'),
-            '\xA0\xA0Move'
-          ]),
-        ]),
-        h('div', [
-          h('button#draw.btn.btn-sm.btn-outline-secondary.mb-1.text-left', {
-            style: {minWidth: '120px'}
-          }, [
-            // https://icons.getbootstrap.com/icons/pencil-fill/
-            bootstrapIcon(
-              'M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z',
-              {marginTop: '-2px'}
-            ),
-            '\xA0\xA0Draw/split'
-          ]),
-        ]),
-        h('div', [
-          h('button#delete.btn.btn-sm.btn-outline-secondary.mb-1.text-left', {
-            style: {minWidth: '120px'}
-          }, [
-            // https://icons.getbootstrap.com/icons/eraser-fill/
-            bootstrapIcon('M8.086 2.207a2 2 0 0 1 2.828 0l3.879 3.879a2 2 0 0 1 0 2.828l-5.5 5.5A2 2 0 0 1 7.879 15H5.12a2 2 0 0 1-1.414-.586l-2.5-2.5a2 2 0 0 1 0-2.828l6.879-6.879zm.66 11.34L3.453 8.254 1.914 9.793a1 1 0 0 0 0 1.414l2.5 2.5a1 1 0 0 0 .707.293H7.88a1 1 0 0 0 .707-.293l.16-.16z'),
-            '\xA0\xA0Delete'
-          ]),
-        ]),
-      ]),
-      h('#draw-walls-hint', 'Press the "Esc" key to stop drawing walls'),
-    ]);
+    return <div id="floorplanner"
+      style={{position: 'relative', height: '300px'}}
+    >
+      <canvas id="floorplanner-canvas" />
+      <div id="floorplanner-controls">
+        <div>
+          <button id="move" className="btn btn-sm btn-outline-secondary mb-1 text-left"
+            style={{minWidth: '120px'}}
+          >
+            {/* https://icons.getbootstrap.com/icons/arrows-move/ */}
+            <BootstrapIcon
+              svgStr="M7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708l2-2zM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10zM.146 8.354a.5.5 0 0 1 0-.708l2-2a.5.5 0 1 1 .708.708L1.707 7.5H5.5a.5.5 0 0 1 0 1H1.707l1.147 1.146a.5.5 0 0 1-.708.708l-2-2zM10 8a.5.5 0 0 1 .5-.5h3.793l-1.147-1.146a.5.5 0 0 1 .708-.708l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L14.293 8.5H10.5A.5.5 0 0 1 10 8z"
+            />
+            &nbsp;&nbsp;Move
+          </button>
+        </div>
+        <div>
+          <button id="draw" className="btn btn-sm btn-outline-secondary mb-1 text-left"
+            style={{minWidth: '120px'}}
+          >
+            {/* https://icons.getbootstrap.com/icons/pencil-fill/ */}
+            <BootstrapIcon
+              svgStr="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"
+              style={{marginTop: '-2px'}}
+            />
+            &nbsp;&nbsp;Draw/split
+          </button>
+        </div>
+        <div>
+          <button id="delete" className="btn btn-sm btn-outline-secondary mb-1 text-left"
+            style={{minWidth: '120px'}}
+          >
+            {/* https://icons.getbootstrap.com/icons/eraser-fill/ */}
+            <BootstrapIcon
+              svgStr="M8.086 2.207a2 2 0 0 1 2.828 0l3.879 3.879a2 2 0 0 1 0 2.828l-5.5 5.5A2 2 0 0 1 7.879 15H5.12a2 2 0 0 1-1.414-.586l-2.5-2.5a2 2 0 0 1 0-2.828l6.879-6.879zm.66 11.34L3.453 8.254 1.914 9.793a1 1 0 0 0 0 1.414l2.5 2.5a1 1 0 0 0 .707.293H7.88a1 1 0 0 0 .707-.293l.16-.16z"
+            />
+            &nbsp;&nbsp;Delete
+          </button>
+        </div>
+      </div>
+      <div id="draw-walls-hint">Press the &quot;Esc&quot; key to stop drawing walls</div>
+    </div>;
   }
 
   componentDidMount() {
